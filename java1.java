b284b94a87f5a4376f074bc42d0716983d49570e
没用的伞 <38153454+qihaodong533@users.noreply.github.com>
2018-06-25 22:19:47 +0800

    修改.java文件
    第一次使用github修改文件

--- java1.java
+++ java1.java
@@ -2,6 +2,7 @@ public class java1
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello world");
+    // System.out.println("Hello world");
+    System.out.println("Hello GitHub");
   }
 }
